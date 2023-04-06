0f941ac8de93f72468507990bd2545644ab72a3e
devSirNaelC7 <sirnaelc7@gmail.com>
2023-04-05 21:58:56 -0300

    API de Cursos

---
+++ cursos/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Avaliacao, Curso
+from .serializers import AvaliacaoSerializer, CursoSerializer
+
+
+class CursoAPIView(APIView):
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+class AvaliacaoAPIView(APIView):
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
