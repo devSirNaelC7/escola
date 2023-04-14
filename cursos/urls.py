318280f56a0915b3807624ceec9c574aab32bd17
devSirNaelC7 <sirnaelc7@gmail.com>
2023-04-14 16:57:39 -0300

    feat: overriding generic views

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,8 +5,13 @@ from .views import (AvaliacaoAPIView, AvaliacoesAPIView, CursoAPIView,
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
-    path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    path('cursos/<int:curso_pk>/avaliacoes/',
+         AvaliacoesAPIView.as_view(), name='curso_avaliaoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/',
+         AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:avaliacao_pk>/',
+         AvaliacaoAPIView.as_view(), name='avaliacao'),
 
 ]
